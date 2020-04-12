8809e4143b7174fca24faa5ee7bac69e04757a40
Nahid Hasan <mnh.nahid35@gmail.com>
2020-04-12 18:27:48 +0600

    colly repo added

--- backEnd/scrap.go
+++ backEnd/scrap.go
@@ -3,6 +3,7 @@ package backEnd
 import (
 	"fmt"
 	"net/http"
+	"github.com/gocolly/colly"
 )
 
 var client = &http.Client{
@@ -10,61 +11,32 @@ var client = &http.Client{
 }
 
 func Scrap(w http.ResponseWriter, r *http.Request) {
+	c := colly.NewCollector(
+	//colly.AllowedDomains("localhost"),
+	)
 
-	fmt.Println("IN scrap")
+	// Find and visit all links
+	c.OnHTML("iframe[src]", func(e *colly.HTMLElement) {
+		desID := e.Attr("src")
+		fmt.Println("Description ID:",desID)
 
-	title, timeLimit, memoryLimit, dNum := getTitle("CodeForces", "831A")
+		// matched, err := regexp.MatchString("problem", res)
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// //fmt.Println(matched)
+		// if matched == true {
+		// 	fmt.Println(res)
+		// }
+	})
 
-	fmt.Println("After getting title ", title)
-	fmt.Println("Finished Scrap")
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
 
-	session, _ := store.Get(r, "mysession")
-	data := map[string]interface{}{
-		"username":    session.Values["username"],
-		"password":    session.Values["password"],
-		"isLogged":    session.Values["isLogin"],
-		"Oj":          "CF",
-		"PNum":        "1111",
-		"PName":       title,
-		"TimeLimit":   timeLimit,
-		"MemoryLimit": memoryLimit,
-		"pageTitle":   title,
-	}
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
 
-	// getting problem description
-	url := "https://vjudge.net" + dNum
-	res, _ := rGET(url)
-
-	fmt.Printf(string(res))
-
-	tpl.ExecuteTemplate(w, "problemView.gohtml", data)
-
-	// c := colly.NewCollector(
-	// //colly.AllowedDomains("localhost"),
-	// )
-
-	// // Find and visit all links
-	// c.OnHTML("iframe[src]", func(e *colly.HTMLElement) {
-	// 	desID := e.Attr("src")
-	// 	fmt.Println("Description ID:",desID)
-
-	// 	// matched, err := regexp.MatchString("problem", res)
-	// 	// if err != nil {
-	// 	// 	panic(err)
-	// 	// }
-	// 	// //fmt.Println(matched)
-	// 	// if matched == true {
-	// 	// 	fmt.Println(res)
-	// 	// }
-	// })
-
-	// c.OnRequest(func(r *colly.Request) {
-	// 	fmt.Println("Visiting", r.URL)
-	// })
-
-	// c.OnScraped(func(r *colly.Response) {
-	// 	fmt.Println("Finished", r.Request.URL)
-	// })
-
-	// c.Visit("https://vjudge.net/problem/CodeForces-341C")
+	c.Visit("https://vjudge.net/problem/CodeForces-341C")
 }
